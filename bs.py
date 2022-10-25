c0e905d030dc1141ff26633f56f4c24941cc1e51
Samnoonmuad <64777888+Samnoonmuad@users.noreply.github.com>
2022-10-25 22:39:23 +0530

    Update bs.py

--- bs.py
+++ bs.py
@@ -1,36 +1,5 @@
 Binary Search Approach: 
 Binary Search is a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(Log n). 
-
-Binary Search Algorithm: The basic steps to perform Binary Search are:
-
-Begin with the mid element of the whole array as a search key.
-If the value of the search key is equal to the item then return an index of the search key.
-Or if the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
-Otherwise, narrow it to the upper half.
-Repeatedly check from the second point until the value is found or the interval is empty.
-Binary Search Algorithm can be implemented in the following two ways
-
-Iterative Method
-Recursive Method
-1. Iteration Method
-
-    binarySearch(arr, x, low, high)
-        repeat till low = high
-               mid = (low + high)/2
-                   if (x == arr[mid])
-                   return mid
-   
-                   else if (x > arr[mid]) // x is on the right side
-                       low = mid + 1
-   
-                   else                  // x is on the left side
-                       high = mid - 1
-2. Recursive Method (The recursive method follows the divide and conquer approach)
-
-    binarySearch(arr, x, low, high)
-           if low > high
-               return False 
-   
            else
                mid = (low + high) / 2 
                    if x == arr[mid]
@@ -106,8 +75,6 @@ def binarySearch(arr, l, r, x):
  
 
     else:
-
-        # Element is not present in the array
 
         return -1
  
